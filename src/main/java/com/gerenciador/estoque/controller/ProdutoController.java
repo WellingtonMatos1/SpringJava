d922fd4f26af18bd7f3d708957fe93a2281b651c
czarframe <cesaragstincs@gmail.com>
2022-09-19 21:37:56 -0300

    Criada a classe ProdutoController

---
+++ src/main/java/com/gerenciador/estoque/controller/ProdutoController.java
@@ -0,0 +1,51 @@
+package com.gerenciador.estoque.controller;
+
+import com.gerenciador.estoque.model.Produto;
+import com.gerenciador.estoque.service.ProdutoService;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ProdutoController {
+    
+    @Autowired
+    private ProdutoService produtoService;
+    
+    @GetMapping("/produtos")
+    public ResponseEntity<List<Produto>> listaProdutos(){
+        return ResponseEntity.status(HttpStatus.OK).body(produtoService.listaProdutos());
+    }
+
+    @GetMapping("produto/{cod_prod}")
+    public ResponseEntity<Optional<Produto>> getByIdProduto(@PathVariable Integer cod_prod){
+        return ResponseEntity.status(HttpStatus.OK).body(produtoService.getByIdProduto(cod_prod));
+    }
+
+    @PostMapping("produto")
+    public ResponseEntity<Produto> salvaProduto(@RequestBody Produto produto){
+        return ResponseEntity.status(HttpStatus.CREATED).body(produtoService.salvaProduto(produto));
+    }
+
+    @PutMapping("produto")
+    public ResponseEntity<Produto> atualizaProduto(@RequestBody Produto produto){
+        return ResponseEntity.status(HttpStatus.OK).body(produtoService.atualizaProduto(produto));
+    }
+    
+    @DeleteMapping("produto/{cod_prod}")
+    public ResponseEntity<String> deleteByIdProduto(@PathVariable Integer cod_prod){
+        produtoService.deleteByIdProduto(cod_prod);
+        return ResponseEntity.status(HttpStatus.OK).body("Produto removido com sucesso");
+    }    
+}
